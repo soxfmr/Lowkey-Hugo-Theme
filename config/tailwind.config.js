4b956e2884de190f59ae81fb16ad39d43636ee98
Soxfmr <6657773+soxfmr@users.noreply.github.com>
2024-08-20 19:35:41 +0800

    Add archives page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['"Inter"', '-apple-system', 'BlinkMacSystemFont', 'avenir next', 'avenir', 'segoe ui', 'helvetica neue', 'helvetica', 'Cantarell', 'Ubuntu', 'roboto', 'noto', 'arial', 'sans-serif'],
+        'sans': ['Helvetica Neue', 'Helvetica', 'Microsoft Yahei'],
       },
     },
   },
